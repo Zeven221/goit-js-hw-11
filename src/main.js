b1262cdaf5dc938126ecec22ce1979445918f06c
zeven <frostdi40@gmail.com>
2026-05-22 21:00:17 +0300

    update

--- src/main.js
+++ src/main.js
@@ -5,18 +5,28 @@ import { renderItems } from './js/render-functions';
 export const refs = {
   formEl: document.querySelector('.js-form'),
   containerElem: document.querySelector('.gallery'),
-  loadingElem: document.querySelector('.loader')
+  loadingElem: document.querySelector('.loader'),
 };
 refs.formEl.addEventListener('submit', e => {
   e.preventDefault();
   const formData = new FormData(refs.formEl);
   const inputValue = formData.get('search-text').trim();
-  if (inputValue.length < 0) {
+  if (inputValue.length === 0) {
+    iziToast.show({
+      close: false,
+      messageColor: '#FFFFFF',
+      message: `Fill please field`,
+      position: 'topRight',
+      progressBar: true,
+      progressBarColor: 'rgb(181, 27, 27)',
+      color: '#EF4040',
+      maxWidth: 432,
+    });
     return;
   }
-  refs.loadingElem.style.display = 'inline-block'
+  refs.loadingElem.style.display = 'inline-block';
   AxiomUserSearch(inputValue).then(res => {
-    refs.loadingElem.style.display = 'none'
+    refs.loadingElem.style.display = 'none';
     if (res.data.hits.length !== 0) {
       renderItems(res.data.hits);
     } else {
@@ -31,5 +41,3 @@ refs.formEl.addEventListener('submit', e => {
     }
   });
 });
-
-
